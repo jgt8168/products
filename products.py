744f886b309b0cc203df4416c1f0af194ae31603
jgt8168 <jgt8168@hotmail.com>
2023-05-26 16:54:04 +0800

    add red file

--- products.py
+++ products.py
@@ -1,4 +1,14 @@
 products=[]
+with open("products.csv",'r', encoding='utf-8') as f:
+    for line in f:
+        if '商品,價格' in line:
+            continue
+        name,price=line.strip().split(',')
+        # print(s)
+        products.append([name,price])
+print(products)
+
+#讓使用者輸入
 while True:
     name=input("請輸入商品名稱:")
     if name=='q':
@@ -12,8 +22,10 @@ while True:
     products.append([name,price])
 print(products)
 # print(products[0][0])
+#印出所有購買紀錄
 for product in products:
     print(product[0],'的價格是',product[1])
+#寫入檔案
 with open('products.csv','w', encoding='utf-8') as f:
     f.write("商品,價格\n")
     for p in products:
